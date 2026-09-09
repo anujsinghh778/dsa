99bc1527bc776820e29d00a437ac8a2e1dded35e
anujsinghh778 <omsinghrajput778772@gmail.com>
2026-09-10 00:27:57 +0530

    Time: 1 ms (34.99%) | Memory: 97.2 MB (99.48%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,39 +1,39 @@
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int i=0;
-//         int j=i+1;
-//         int profit = 0;
-//         int maxprofit = 0; 
-//         while(j<prices.size()){
-//             profit=prices[j]-prices[i];
-//             maxprofit=max(profit,maxprofit);
-//             if(profit<=0){
-//                 i++;
-//                 j++;
-//             }
-//             else{
-//                 j++;
-//             }
-//         }
-//         return maxprofit;
-//     }
-// };
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int i = 0;
-        int maxprofit = 0;
-
-        for(int j = 1; j < prices.size(); j++) {
-            if(prices[j] < prices[i]) {
-                i = j;
+        int i=0;
+        int j=i+1;
+        int profit = 0;
+        int maxprofit = 0; 
+        while(j<prices.size()){
+            profit=prices[j]-prices[i];
+            maxprofit=max(profit,maxprofit);
+            if(profit<=0){
+                i=j;
+                j++;
             }
-            else {
-                maxprofit = max(maxprofit, prices[j] - prices[i]);
+            else{
+                j++;
             }
         }
-
         return maxprofit;
     }
 };
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int i = 0;
+//         int maxprofit = 0;
+
+//         for(int j = 1; j < prices.size(); j++) {
+//             if(prices[j] < prices[i]) {
+//                 i = j;
+//             }
+//             else {
+//                 maxprofit = max(maxprofit, prices[j] - prices[i]);
+//             }
+//         }
+
+//         return maxprofit;
+//     }
+// };
